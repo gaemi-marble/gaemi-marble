c90279c42cf6c8d6bab46ff93427aa184cdc9382
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-10-16 15:18:59 +0900

    feat: 프로젝트 초기 설정 #2

---
+++ be/src/main/java/codesquad/gaemimarble/GaemiMarbleApplication.java
@@ -0,0 +1,13 @@
+package codesquad.gaemimarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaemiMarbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaemiMarbleApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/codesquad/gaemimarble/GaemiMarbleApplicationTests.java
@@ -0,0 +1,13 @@
+package codesquad.gaemimarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaemiMarbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
