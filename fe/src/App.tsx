3fd85b5be030db8d62b82d37d801e1a5a4814185
aaaz425 <aaaz425@naver.com>
2023-10-17 10:58:17 +0900

    refactor: 라우터 분리

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,42 +1,17 @@
-import { styled } from 'styled-components';
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/designSystem";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
+import { Page } from "./styles/common";
 
-function App() {
+export default function App() {
   return (
-    <StyledApp>
-      <Title>Gaemi Marble</Title>
-      <Button>게임시작</Button>
-    </StyledApp>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Page>
+        <RouterProvider router={router} />
+      </Page>
+    </ThemeProvider>
   );
 }
-
-const StyledApp = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  background-color: royalblue;
-`;
-
-const Title = styled.h1`
-  font-size: 64px;
-`;
-
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-  border: 1px solid white;
-  border-radius: 10px;
-  font-size: 32px;
-  cursor: pointer;
-
-  &:hover {
-    color: black;
-    border-color: black;
-    background-color: #e5e5e5;
-  }
-`;
-
-export default App;
--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { GlobalStyle } from './styles/GlobalStyle.ts';
-import Home from './components/Home.tsx';
-import Login from './components/Login.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/signin',
-    element: <Login />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
