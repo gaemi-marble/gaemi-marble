dce0a6df37affed4a6d6125d32aa636146845cd5
aaaz425 <aaaz425@naver.com>
2023-10-19 12:55:19 +0900

    refactor: 사용자 인증 페이지 Layout 적용

--- fe/src/styles/common.ts
+++ fe/src/styles/common.ts
@@ -1,12 +1,5 @@
 import { styled } from 'styled-components';
 
-export const Page = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
 export const ModalRoot = styled.div`
   position: fixed;
   top: 0;
