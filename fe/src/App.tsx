8e740fbcde39020677b08ee3f511118509507eef
aaaz425 <aaaz425@naver.com>
2023-10-19 11:15:02 +0900

    feat: modal 루트 추가

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { router } from './router/router';
 import { GlobalStyle } from './styles/GlobalStyle';
-import { Page } from './styles/common';
+import { ModalRoot, Page } from './styles/common';
 import { theme } from './styles/designSystem';
 
 export default function App() {
@@ -12,6 +12,7 @@ export default function App() {
       <Page>
         <RouterProvider router={router} />
       </Page>
+      <ModalRoot id="modal-root" />
     </ThemeProvider>
   );
 }
--- fe/src/styles/common.ts
+++ fe/src/styles/common.ts
@@ -3,44 +3,21 @@ import { styled } from 'styled-components';
 export const Page = styled.div`
   width: 100vw;
   height: 100vh;
-`;
-
-// TODO: Container 네이밍 변경
-export const Container = styled.div`
-  width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-  color: ${({ theme: { color } }) => color.accentText};
-  background-color: ${({ theme: { color } }) => color.accentPrimary};
 `;
 
-export const Title = styled.h1`
-  font-size: ${({ theme: { fontSize } }) => fontSize.xLarge};
+export const ModalRoot = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 100;
 `;
 
-// TODO: Input 공통 컴포넌트로 만들기
-export const Input = styled.input`
-  padding: 0 6px;
-  border-radius: ${({ theme: { radius } }) => radius.small};
-  color: ${({ theme: { color } }) => color.neutralText};
-`;
-
-// TODO: Button 공통 컴포넌트로 만들기
-export const Button = styled.button`
-  width: 200px;
-  height: 100px;
-  border: ${({ theme: { color } }) => `1px solid ${color.accentBorder}`};
-  border-radius: ${({ theme: { radius } }) => radius.medium};
-  font-size: ${({ theme: { fontSize } }) => fontSize.large};
-  cursor: pointer;
-
-  &:hover {
-    border-color: ${({ theme: { color } }) => color.neutralBorderStrong};
-    color: ${({ theme: { color } }) => color.neutralTextStrong};
-    background-color: ${({ theme: { color } }) => color.neutralBackground};
-  }
+export const Dim = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme: { color } }) => color.neutralOverlay};
+  z-index: -1;
 `;
