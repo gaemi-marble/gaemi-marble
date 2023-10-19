ef86e4c5e64dc413eb1aa823a86f3b0a7dd3aa89
aaaz425 <aaaz425@naver.com>
2023-10-19 13:00:42 +0900

    refactor: 필요없는 fragment 삭제

--- fe/src/router/router.tsx
+++ fe/src/router/router.tsx
@@ -13,25 +13,21 @@ import { ROUTE_PATH } from './constants';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route element={<Layout />}>
-        <Route
-          element={
-            <ProtectedRoute needAuth={false} redirectRoute={ROUTE_PATH.HOME} />
-          }
-        >
-          <Route path={ROUTE_PATH.SIGNIN} element={<SignInPage />} />
-          <Route path={ROUTE_PATH.SIGNUP} element={<SignUpPage />} />
-        </Route>
-        <Route
-          element={
-            <ProtectedRoute needAuth redirectRoute={ROUTE_PATH.SIGNIN} />
-          }
-        >
-          <Route path={ROUTE_PATH.HOME} element={<HomePage />} />
-          <Route path={`${ROUTE_PATH.GAME}/:gameId`} element={<GamePage />} />
-        </Route>
+    <Route element={<Layout />}>
+      <Route
+        element={
+          <ProtectedRoute needAuth={false} redirectRoute={ROUTE_PATH.HOME} />
+        }
+      >
+        <Route path={ROUTE_PATH.SIGNIN} element={<SignInPage />} />
+        <Route path={ROUTE_PATH.SIGNUP} element={<SignUpPage />} />
       </Route>
-    </>
+      <Route
+        element={<ProtectedRoute needAuth redirectRoute={ROUTE_PATH.SIGNIN} />}
+      >
+        <Route path={ROUTE_PATH.HOME} element={<HomePage />} />
+        <Route path={`${ROUTE_PATH.GAME}/:gameId`} element={<GamePage />} />
+      </Route>
+    </Route>
   )
 );
