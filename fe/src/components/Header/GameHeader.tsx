369dc755db4f06e77d9e1c244cfaca08f8441beb
aaaz425 <aaaz425@naver.com>
2023-10-20 11:40:56 +0900

    refactor: 페이지별 헤더 구분

---
+++ fe/src/components/Header/GameHeader.tsx
@@ -0,0 +1,63 @@
+import { Icon } from '@components/icon/Icon';
+import { styled } from 'styled-components';
+
+type GameHeaderProps = {
+  handleClickTest: () => void;
+};
+
+export default function GameHeader({ handleClickTest }: GameHeaderProps) {
+  return (
+    <Header>
+      <Logo>Gaemi Marble</Logo>
+      <Temp>
+        <IconContainer>
+          <Icon
+            name="sample"
+            size="3rem"
+            color="neutralText"
+            onClick={handleClickTest}
+          />
+        </IconContainer>
+        <IconContainer>
+          <Icon name="exit" size="3rem" color="accentText" />
+        </IconContainer>
+      </Temp>
+    </Header>
+  );
+}
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  position: fixed;
+  top: 0.5rem;
+  padding: 0 2rem;
+  justify-content: space-between;
+`;
+
+const Logo = styled.h1`
+  font-size: ${({ theme: { fontSize } }) => fontSize.medium};
+  color: ${({ theme: { color } }) => color.accentText};
+`;
+
+// Todo: have to delete!!!
+const Temp = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const IconContainer = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border-radius: ${({ theme: { radius } }) => radius.half};
+  background-color: ${({ theme: { color } }) => color.neutralBackground};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme: { color } }) => color.accentSecondary};
+
+    svg path {
+      fill: ${({ theme: { color } }) => color.accentText};
+    }
+  }
+`;
