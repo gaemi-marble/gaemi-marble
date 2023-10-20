3ceb1f805c09851d445206af1f0b865f1b5b83bb
fuse <massenger7@gmail.com>
2023-10-20 09:29:22 +0900

    add: 몇몇 기업 로고 이미지와 특수칸 이미지 파일 추가

---
+++ fe/src/assets/images/index.ts
@@ -0,0 +1,31 @@
+import apple from './apple.png';
+import goldCard from './gold-card.png';
+import google from './google.png';
+import hermes from './hermes.png';
+import jail from './jail.png';
+import mcdonalds from './mcdonalds.png';
+import rocket from './rocket.png';
+import start from './start-arrow.png';
+import goodNews from './stock-up.png';
+import tax from './tax.png';
+import tesla from './tesla.png';
+import twitter from './twitter.png';
+
+type CategoryIconMap = {
+  [key: string]: string;
+};
+
+export const categoryIconMap: CategoryIconMap = {
+  google,
+  apple,
+  twitter,
+  tesla,
+  mcdonalds,
+  hermes,
+  jail,
+  rocket,
+  start,
+  goodNews,
+  tax,
+  goldCard,
+};
