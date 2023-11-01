bbda83850a66f0e4d1730cf36384981a00d1b035
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-11-01 15:54:28 +0900

    feat: 강탈 기능, 개인 현황판 수정 #65

---
+++ be/src/main/java/codesquad/gaemimarble/game/dto/request/GameRobRequest.java
@@ -0,0 +1,20 @@
+package codesquad.gaemimarble.game.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GameRobRequest {
+	private Long gameId;
+	private String playerId;
+	private String targetId;
+
+	@Builder
+	private GameRobRequest(Long gameId, String playerId, String targetId) {
+		this.gameId = gameId;
+		this.playerId = playerId;
+		this.targetId = targetId;
+	}
+}
