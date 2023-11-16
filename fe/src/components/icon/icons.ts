0d61b18adc0f60b2c1adcdf39d670a13903f013f
aaaz425 <aaaz425@naver.com>
2023-11-16 15:33:10 +0900

    merge: git 충돌 커밋 반영

--- fe/src/components/icon/icons.ts
+++ fe/src/components/icon/icons.ts
@@ -1,3 +1,4 @@
+import angry from '@assets/icon/angry.svg?react';
 import antBasic from '@assets/icon/antBasic.svg?react';
 import antFire from '@assets/icon/antFire.svg?react';
 import antHoney from '@assets/icon/antHoney.svg?react';
@@ -6,8 +7,14 @@ import arrowDown from '@assets/icon/arrowDown.svg?react';
 import arrowLeft from '@assets/icon/arrowLeft.svg?react';
 import arrowRight from '@assets/icon/arrowRight.svg?react';
 import arrowUp from '@assets/icon/arrowUp.svg?react';
+import celebrate from '@assets/icon/celebrate.svg?react';
+import clock from '@assets/icon/clock.svg?react';
+import cry from '@assets/icon/cry.svg?react';
+import emote from '@assets/icon/emote.svg?react';
 import enter from '@assets/icon/enter.svg?react';
 import exit from '@assets/icon/exit.svg?react';
+import hi from '@assets/icon/hi.svg?react';
+import laugh from '@assets/icon/laugh.svg?react';
 import plus from '@assets/icon/plus.svg?react';
 import refresh from '@assets/icon/refresh.svg?react';
 import sample from '@assets/icon/sample.svg?react';
@@ -32,6 +39,13 @@ export const icons = {
   soundMute,
   soundPlaying,
   statusBoard,
+  emote,
+  angry,
+  celebrate,
+  cry,
+  hi,
+  laugh,
+  clock,
 };
 
 export type IconsType = keyof typeof icons;
